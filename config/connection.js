c0567cfcbd62d734244241488733ac7e07d30c97
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-05-14 18:12:34 -0400

    changed

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection;
 
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if(process.env.CLEARDB_DATABASE_URL){
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 }
 else{
   connection = mysql.createConnection({
